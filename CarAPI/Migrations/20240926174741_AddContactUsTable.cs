0b0592a3ffde9a06f10239be51f11be52a1da881
Ifeoluwa Janet Ibukun-ojo <iibukunojo@gmail.com>
2024-09-26 18:53:23 +0100

    push

---
+++ CarAPI/Migrations/20240926174741_AddContactUsTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactUsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    RequesterName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequesterEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateRequested = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarRequests_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactUs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubmittedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactUs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarRequests_CarId",
+                table: "CarRequests",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarRequests");
+
+            migrationBuilder.DropTable(
+                name: "ContactUs");
+        }
+    }
+}
